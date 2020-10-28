811e18dd03da3a9c9d92393f8fe3e8d6bc326309
SuryaTeja-koka <66080644+SuryaTeja-koka@users.noreply.github.com>
2020-10-28 14:43:32 +0530

    Update 010. Weather Observation Station 5.sql

--- 010. Weather Observation Station 5.sql
+++ 010. Weather Observation Station 5.sql
@@ -1,17 +1,9 @@
 -- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
 -- # Score: 30
 
-
-SELECT *
-FROM
-    (SELECT CITY, LENGTH(CITY)
-    FROM STATION
-    ORDER BY LENGTH(CITY), CITY)
-WHERE ROWNUM = 1
-UNION
-SELECT *
-FROM
-    (SELECT CITY, LENGTH(CITY)
-    FROM STATION
-    ORDER BY LENGTH(CITY) DESC, CITY)
-WHERE ROWNUM = 1;
+select city, length(city) from station
+order by length(city),city asc
+limit 1;
+select city, length(city) from station
+order by length(city) desc
+limit 1;
